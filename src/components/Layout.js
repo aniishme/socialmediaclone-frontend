908dd6b5abdb2993f4f98f4417b711a0d0274d36
Anish Sharma <sharmaanish023@gmail.com>
2022-06-12 20:29:48 +0545

    Post, Interaction component spacing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -58,9 +58,7 @@ function Layout({ children, style }) {
             </div>
           </div>
           <div className="content-wrapper">{children}</div>
-          <div className="rightbar-wrapper">
-            <h3>People You May Know</h3>
-          </div>
+          <div className="rightbar-wrapper"></div>
         </div>
       </div>
     </div>
--- src/components/Post.js
+++ src/components/Post.js
@@ -6,26 +6,30 @@ import ProfileImage from "./ProfileImage";
 function Post() {
   const postObj = {
     title: "This is my first post here...",
-    image: "",
+    image:
+      "https://pbs.twimg.com/profile_images/1322220842529468424/1yDqI8ti_400x400.jpg",
     likes: 1,
     comments: [],
   };
   return (
     <div className="post-wrapper">
-      <div className="post-profile">
-        <div className="post-profile-image">
-          <ProfileImage size={50} />
+      <div className="post-content">
+        <div className="post-profile">
+          <div className="post-profile-image">
+            <ProfileImage size={50} />
+          </div>
+          <h4 className="post-profile-name">Anish Sharma</h4>
         </div>
-        <h4 className="post-profile-name">Anish Sharma</h4>
+        <div className="post-title">{postObj.title}</div>
+        {postObj.image ? (
+          <div className="post-image-wrapper">
+            <img src={postObj.image} alt={postObj.title} />
+          </div>
+        ) : (
+          ""
+        )}
       </div>
-      <div className="post-title">{postObj.title}</div>
-      {postObj.image ? (
-        <div className="post-image-wrapper">
-          <img src={postObj.image} alt={postObj.title} />
-        </div>
-      ) : (
-        ""
-      )}
+
       <Interaction />
     </div>
   );
